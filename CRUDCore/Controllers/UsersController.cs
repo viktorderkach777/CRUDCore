c4f892c55fe43431d7f444e15cf76232252bc45b
viktorderkach777 <viktorderkach777@yandex.ua>
2019-06-06 21:01:31 +0300

    add transcope

--- CRUDCore/Controllers/UsersController.cs
+++ CRUDCore/Controllers/UsersController.cs
@@ -3,31 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
 using CRUDCore.DAL.Entities;
 using CRUDCore.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CRUDCore.Controllers
-{ 
-    [Produces("application/json")]
-[Route("api/[controller]")]
-//[ApiController]
-public class UsersController : ControllerBase
 {
-
-    private readonly EFContext _context;
-
-    public UsersController(EFContext context)
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class UsersController : ControllerBase
     {
-        _context = context;
-    }
 
-    [HttpGet]
-    public List<UserItemViewModel> GetUsers()
-    {
-       Thread.Sleep(2000);  
+        private readonly EFContext _context;
+        private readonly UserManager<DbUser> _userManager;
+        private readonly RoleManager<DbRole> _roleManager;
+
+        public UsersController(UserManager<DbUser> userManager,
+                   RoleManager<DbRole> roleManager, EFContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<UserItemViewModel> GetUsers()
+        {
+            Thread.Sleep(500);
             var el = _context.Set<DbUser>().FirstOrDefault(s => s.Email == "admin@gmail.com");
             var model = new List<UserItemViewModel>
             {
@@ -49,39 +56,82 @@ public class UsersController : ControllerBase
                     }
                 }
             };
-        return model;
+            return model;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody]UserItemViewModel model)
+        {
+            try
+            {
+                using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    DbUser dbUser = new DbUser
+                    {
+                        Email = model.Email,
+                        Age = model.Age,
+                        Image = model.Image,
+                        Description = model.Description,
+                        Phone = model.Phone,
+                        UserName = model.Email
+                    };
+                    var result = await _userManager.CreateAsync(dbUser, "Qwerty1-");
+                    if (!result.Succeeded)
+                        throw new Exception("Problem create user");
+                    foreach (var role in model.Roles)
+                    {
+                        var roleresult = await _roleManager.CreateAsync(new DbRole
+                        {
+                            Name = role.Name
+
+                        });
+                        if (!roleresult.Succeeded)
+                            throw new Exception("Problem create role");
+                    }
+                    throw new Exception("my exception crash");
+                    scope.Complete();
+                    return Ok(dbUser.Id);
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+
+
+            
+        }
+
+
+        // GET: api/<controller>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<controller>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<controller>
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
-
-    // GET: api/<controller>
-    //[HttpGet]
-    //public IEnumerable<string> Get()
-    //{
-    //    return new string[] { "value1", "value2" };
-    //}
-
-    //// GET api/<controller>/5
-    //[HttpGet("{id}")]
-    //public string Get(int id)
-    //{
-    //    return "value";
-    //}
-
-    //// POST api/<controller>
-    //[HttpPost]
-    //public void Post([FromBody]string value)
-    //{
-    //}
-
-    //// PUT api/<controller>/5
-    //[HttpPut("{id}")]
-    //public void Put(int id, [FromBody]string value)
-    //{
-    //}
-
-    //// DELETE api/<controller>/5
-    //[HttpDelete("{id}")]
-    //public void Delete(int id)
-    //{
-    //}
-}
 }
